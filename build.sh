e193b19c8772fd7f85d7f7301ebe804711c9d89e
sharkov <sharkov@ispras.ru>
2023-10-30 14:19:52 +0300

    added build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+dpkg -i aksusbd_8.52-1_amd64.deb
+
 cd /src
+
 /svace/bin/svace build --init mvn clean install
 /svace/bin/svace analyze
+
+tar -czvf svace-dir.tar.gz .svace-dir
+rm -fr .svace-dir
+
+/codescoring/johnny scan dir . --project $JOB_NAME --create-project --save-results \
+--api_token $TOKEN --api_url $URL --with-hashes --ignore .git
