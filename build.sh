bef37318e56cc4494761c3a76a3f6dc069f485ef
sharkov <sharkov@ispras.ru>
2023-10-30 15:32:21 +0300

    zzzzz

--- build.sh
+++ build.sh
@@ -4,11 +4,7 @@ dpkg -i aksusbd_8.52-1_amd64.deb
 cd /src
 
 /svace/bin/svace build --init mvn clean install
-RES = $(/svace/bin/svace analyze)
-
-echo Svace analyze has terminated with: $RES
-# tar cvpzf svace-dir.tar.gz .svace-dir
-# rm -fr .svace-dir
+/svace/bin/svace analyze && tar cvpzf svace-dir.tar.gz .svace-dir && rm -fr .svace-dir
 
 /codescoring/johnny scan dir . --project $JOB_NAME --create-project --save-results \
 --api_token $TOKEN --api_url $URL --with-hashes --ignore .git
